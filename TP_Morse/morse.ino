3ccf24338253535d2a65ba487bd527d47d34b015
YasmineBOU <yasmineboudjemai10@gmail.com>
2020-03-09 22:18:29 +0100

    Second Part of Morse done: From Arduibo to PC

--- TP_Morse/morse.ino
+++ TP_Morse/morse.ino
@@ -1,56 +1,119 @@
 /*
 
 */
+#include <assert.h>
+#include <stdlib.h>
 
 #define BUTTON_PIN 5
 #define BUZZER_PIN 6
 
-#define LETTER_DELIMITER 52  //'4' - '0'
-#define SHORT_TONE_VALUE 49  //'1' - '0' 
-#define LONG_TONE_VALUE 50   //'2' - '0' 
+#define LETTER_DELIMITER '4'
+#define SHORT_TONE_VALUE '1'
+#define LONG_TONE_VALUE '2'
 #define SHORT_TONE_TIME 200
 #define FREQUENCE 1000
 
+#define PC_MODE '8'
+#define ARDUINO_MODE '9'
+
+#define MSG_STEP_SIZE 100
+
+
+bool arduinoMode = true;
+
+const char letters[]        = {'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                               'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                               'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                               'V', 'W', 'X', 'Y', 'Z',
+
+                               '1', '2', '3', '4', '5',
+                               '6', '7', '8', '9', '0',
+
+                               ' '
+                              };
+
+int lettersCode[][6]        = {{49, 50}, {500, 49, 49, 49}, {50, 49, 50, 49}, {50, 49, 49}, {49}, {49, 49, 50, 49}, {50, 50, 49},
+  {49, 49, 49, 49}, {49, 49}, {49, 50, 50, 50}, {50, 49, 50}, {49, 50, 49, 49}, {50, 50}, {50, 49},
+  {50, 50, 50}, {49, 50, 50, 49}, {50, 50, 49, 50}, {49, 50, 49}, {49, 49, 49}, {50}, {49, 49, 50},
+  {49, 49, 49, 50}, {49, 50, 50}, {50, 49, 49, 50}, {50, 49, 50, 50}, {50, 50, 49, 49},
+
+  {49, 50, 50, 50, 50}, {49, 49, 50, 50, 50}, {49, 49, 49, 50, 50}, {49, 49, 49, 49, 50}, {49, 49, 49, 49, 49},
+  {50, 49, 49, 49, 49}, {50, 50, 49, 49, 49}, {50, 50, 50, 49, 49}, {50, 50, 50, 50, 49}, {50, 50, 50, 50, 50},
+
+  {49, 49, 49, 49, 49, 49}
+};
+
+static char *_message       = NULL;
+static int _msgCurrentSize  = 0;
+static int _msgMaxSize      = MSG_STEP_SIZE;
+
+
 void setup() {
+
+  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
-  Serial.begin(9600);
+  _message = malloc(_msgMaxSize);
+  assert(_message);
+  
 }
 
 
-void loop() {
-  
-  if (!Serial.available()) return;
+char decode(int values[10], int size) {
 
-  for (int value = Serial.read(); value != LETTER_DELIMITER; value = Serial.read()) { // 52 the ascii code of the number 4 that is a delimiter of letters
-    Serial.print(String("***Value: ") + value + "\n");
-
-    if (value == SHORT_TONE_VALUE) {
-      tone(BUZZER_PIN, FREQUENCE); 
-      delay(SHORT_TONE_TIME);
-      //Serial.println("==> SHORT TONE");
-      noTone(BUZZER_PIN);
-      delay(100);
-    }
-
-    else if (value == LONG_TONE_VALUE) {
-      tone(BUZZER_PIN, FREQUENCE); 
-      delay(SHORT_TONE_TIME * 2);
-      //Serial.println("==> LONG TONE");
-      noTone(BUZZER_PIN);
-      delay(100);
-
-    }
-
-    else {
-      noTone(BUZZER_PIN);
-      delay(SHORT_TONE_TIME);
-      //Serial.println("==> NO TONE");
-    }
+  for (int i = 0; i < size; ++i) {
 
   }
 
-  Serial.println("Another letter\n\n");
-  delay(100);
+}
 
+void printMessage(char letter) {
+
+  if (_msgCurrentSize >= _msgMaxSize) {
+    _msgMaxSize += MSG_STEP_SIZE;
+    _message = realloc(_message, _msgMaxSize);
+    assert(_message);
+  }
+
+  _message[_msgCurrentSize] = letter;
+  Serial.println(_message);
+}
+
+void loop() {
+  
+	Serial.println(digitalRead(BUTTON_PIN));
+	
+	if (!Serial.available()) return;
+
+	int values[10] = {0}, i = 0;
+
+
+	for (int value = Serial.read(); value != LETTER_DELIMITER; value = Serial.read(), ++i) { // 52 the ascii code of the number 4 that is a delimiter of letters
+
+		if (value == SHORT_TONE_VALUE) {
+		  values[i] = value;
+		  tone(BUZZER_PIN, FREQUENCE);
+		  delay(SHORT_TONE_TIME);
+		  noTone(BUZZER_PIN);
+		  delay(100);
+		}
+
+		else if (value == LONG_TONE_VALUE) {
+		  values[i] = value;
+		  tone(BUZZER_PIN, FREQUENCE);
+		  delay(SHORT_TONE_TIME * 2);
+		  noTone(BUZZER_PIN);
+		  delay(100);
+
+		}
+
+		else {
+		  noTone(BUZZER_PIN);
+		  delay(SHORT_TONE_TIME);
+		}
+
+	}
+
+	delay(100);
+  
 }
--- TP_Morse/morse.py
+++ TP_Morse/morse.py
@@ -8,17 +8,30 @@ from getkey import getkey
 # Just for DEBUG
 from pprint import pprint
 
+CMD_NB_ARGUMENTS    = 3
 USB_PORT_NUM        = 9600
 MAX_DURATION        = 10  # express the duration that elpased from the last "1" to the actual "1"
-LETTERS_AND_CODE    = {"A": "-_", "B": "_---", "C": "_-_-", "D": "_--", "E": "-"}
+LETTERS_AND_CODE    = { "A": "-_", "B": "_---", "C": "_-_-", "D": "_--", "E": "-",
+                        "F": "--_-", "G": "__-", "H": "----", "I": "--", "J": "-___",
+                        "K": "_-_", "L": "-_--", "M": "__", "N": "_-", "O": "___",
+                        "P": "-__-", "Q": "__-_", "R": "-_-", "S": "---", "T": "_",
+                        "U": "--_", "V": "---_", "W": "-__", "X": "_--_", "Y": "_-__",
+                        "Z": "__--",
+
+                        "1": "-____", "2": "--___", "3": "---__", "4": "----_", "5": "-----",
+                        "6": "_----", "7": "__---", "8": "___--", "9": "____-", "0": "_____" 
+                    }
+
 CODE_VALUE          = {"-": "1", "_": "2"}
-CMD_NB_ARGUMENTS    = 3
 LETTER_DELIMITER    = len(CODE_VALUE) + 2
 
+SHORT_TONE_DURATION = 0.17
+BET_TONES_DURATION  = 0.6
+BET_WORDS_DURATION  = 1.6
 
+PC_MODE             = '8'
+ARDUINO_MODE        = '9'
 
-def getIndexNextSignal(signalList, currentIndex, currentSignal):
-    return [index for index, (signal, seconds) in enumerate(signalList[currentIndex: ]) if signal != currentSignal][0]
 
 
 def getElapsedTime(signalList):
@@ -26,36 +39,100 @@ def getElapsedTime(signalList):
     lastSignalSeen  = signalList[0][0]
     timeElapsed     = signalList[0][1]
     timeElapsedList = []
+    length          = len(signalList) - 1
 
     for index, (signal, seconds) in enumerate(signalList):
+        if index == length:
+            timeElapsedList.append((lastSignalSeen, seconds - timeElapsed))
+
         if signal != lastSignalSeen:
-            timeElapsedList.append((signal, signalList[index][1] - timeElapsed))
-            timeElapsed  = seconds 
+            timeElapsedList.append((lastSignalSeen, signalList[index - 1][1] - timeElapsed))
+            timeElapsed     = seconds 
+            lastSignalSeen  = signal
         
     return timeElapsedList
 
 
-def getSignalsIndices(signalList):
-    indices = [0]
-    lastSignalSeen = -1
-    for index, (signal, _) in enumerate(signalList):
-        if lastSignalSeen == signal:
-            continue
-        else:
-            lastSignalSeen = signal
-            indices.append((signal, getIndexNextSignal(signalList, index, signal)))
+def letterFinished(timeElapsedList, index, signal=1):
+
+    # check if we are in the bounds of the list with (index -2) otherwise we'll get errors
+    # (index - 2) because each element of the list is different from its previous (ie: [(0, 4.56), (1, 0.67), (0, 0.62), (1, 0.08)]) 
+    if signal == 1:
+        return (timeElapsedList[index - 1][1] >= BET_TONES_DURATION and timeElapsedList[index - 1][1] < BET_WORDS_DURATION) if (index - 2) >= 0 else False
+    else:
+        return (timeElapsedList[index][1] >= BET_TONES_DURATION and timeElapsedList[index][1] < BET_WORDS_DURATION)
+
+
+def wordFinished(timeElapsedList, index, signal=1):
+
+    # check if we are in the bounds of the list with (index -2) otherwise we'll get errors
+    # (index - 2) because each element of the list is different from its previous (ie: [(0, 4.56), (1, 0.67), (0, 0.62), (1, 0.08)]) 
+    if signal == 1:
+        return  timeElapsedList[index - 1][1] >= BET_WORDS_DURATION if (index - 2) >= 0 else False
+    else:
+        return  timeElapsedList[index][1] >= BET_WORDS_DURATION 
 
 
 
-
-def decode(signalList):
+def decodeSignal(signalList):
+    
     timeElapsedList = getElapsedTime(signalList)
     output = ""
 
-    for signal, timeElapsed in timeElapsedList:
-        output += "-" if timeElapsed < MAX_DURATION else "_"
+    for index, (signal, timeElapsed) in enumerate(timeElapsedList):
+        if index == 0:
+            continue
+
+        if wordFinished(timeElapsedList, index, 0):
+            output += "w"
+
+        elif letterFinished(timeElapsedList, index, 0):
+            output += "l"
+        
+        if signal == 1:
+           
+            output += "-" if timeElapsed < SHORT_TONE_DURATION else "_"
 
     return output
+
+
+def getKeyByValue(targetedValue):
+
+    for key, value in LETTERS_AND_CODE.items():
+        if value == targetedValue:
+            return key
+    return None
+
+
+
+def interpretDecodedSignal(decodedSignal):
+
+    letterCode, output = '', ''
+    delimiters = ['l', 'w']
+
+    for char in decodedSignal:
+        if char in delimiters:
+            letter = getKeyByValue(letterCode)
+            output += letter if letter else ''
+            letterCode = ''
+            if char == 'w':
+                output += ' '
+        else:
+            letterCode += char
+
+    return output
+
+
+
+def getNbCharsInString(string, charsList):
+    
+    nb = 0
+    for char in string:
+        if char in charsList:
+            nb += 1
+
+    return nb
+
 
 
 if __name__ == "__main__":
@@ -66,18 +143,21 @@ if __name__ == "__main__":
 
     USBPortName = sys.argv[1]
     ser         = serial.Serial(USBPortName, USB_PORT_NUM)
-    action      = sys.argv[2]
+    action      = sys.argv[2].lower()
+    keyPressed  = ""
     
-    if action.lower() == "pc":
-        keyPressed = ""
-        while(keyPressed.lower() != "\x1b"):
+    if action == "pc":
+
+        ser.write(bytes(ARDUINO_MODE, "utf8"))
+
+        while keyPressed.lower() != "\x1b":
             keyPressed = getkey().upper()
             print("The key pressed is : " + repr(keyPressed))
             if keyPressed in LETTERS_AND_CODE.keys():
                 letterCode = LETTERS_AND_CODE[keyPressed]
                 for char in letterCode:
                     charCode = CODE_VALUE[char]
-                    print('{} ==> {}'.format(
+                    print('\t{} ==> {}'.format(
                         char,
                         charCode
                     ))
@@ -87,22 +167,80 @@ if __name__ == "__main__":
             
             ser.write(bytes(str(LETTER_DELIMITER), 'utf8'))
 
-
-                    # ser.write(0)
            
     else: 
 
-        tStart = time.time()
-        output = []
-        maxTime = 10
-        while((time.time() - tStart) < maxTime):
+        ser.write(bytes(PC_MODE, "utf8"))
+
+        tStart      = time.time()
+        output      = [(0, 0.0)]
+        maxTime     = 15
+        prevMsgSize = 0
+        curMsgSize  = 0
+
+        while True :   
+
             value = [int(car) for car in ser.read().split() if car.isdigit()]
             value = value[0] if value else -1
             if value == 0 or value == 1:
                 output.append((value, time.time() - tStart))
-                # print("Value got {}".format(value))
 
-        
+
+            decodedSignal   = decodeSignal(output)
+            message         = interpretDecodedSignal(decodedSignal)
+            curMsgSize      = len(message)
+
+            if curMsgSize != prevMsgSize:
+                prevMsgSize = curMsgSize
+                print("\033c", end="")
+                print("Message: {}".format(message))
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # currentSize = len(decodedSignal)
+        # if currentSize != previousSize:     
+        #     # print("decodeSignal: {}".format(decodedSignal))
+
+        #     previousSize = currentSize
+        #     curNbLetterWordDelimiter = getNbCharsInString(decodedSignal, ['l', 'w'])    
+            
+        #     if curNbLetterWordDelimiter != prevNbLetterWordDelimiter:
+        #         prevNbLetterWordDelimiter = curNbLetterWordDelimiter
+
+        #         message = interpretDecodedSignal(decodedSignal)
+        #         print("decodedSignal: {} ==> {}".format(decodedSignal, message))
                 
-        pprint(output)
-        print(decode(output))
+        # pprint(output)
+        # print("********************Decode**********************")
+        # print(decode(output))
