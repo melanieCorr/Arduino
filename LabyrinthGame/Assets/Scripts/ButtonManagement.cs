3cfe96bbb927a8c4f5abd642c9412308ebac252c
MelanieCorr <mel.correia.92@gmail.com>
2020-03-24 14:22:18 +0100

    Ajout d'un menu principal

---
+++ LabyrinthGame/Assets/Scripts/ButtonManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManagement : MonoBehaviour
+{
+   public void QuitButton() {
+       // Quitter le jeu
+       Debug.Log("Quitter le jeu.");
+       Application.Quit();
+   }
+
+   public void StartButton() {
+       // Lancer le jeu
+       Debug.Log("Lancer le jeu.");
+       Application.LoadLevel("GameScene");
+   }
+
+   public void RulesButton() {
+       // Règles du jeu
+       Debug.Log("Règles du jeu.");
+       
+   }
+}
