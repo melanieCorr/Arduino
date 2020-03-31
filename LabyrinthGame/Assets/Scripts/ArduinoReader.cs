49f7fa0b176175d817dcd85ea9569d285c8a7c9a
YasmineBOU <yasmineboudjemai10@gmail.com>
2020-03-31 12:15:09 +0200

    Character added : Version not working well

---
+++ LabyrinthGame/Assets/Scripts/ArduinoReader.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO.Ports;
+using System.Threading;
+using System;
+
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public class ArduinoReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SerialPort serialPort;
+    public int[] coordXYZ = { 0, 0, 0};
+    public int[] coefXYZ  = {0, 0, 0}; // represents the direction to take
+    public void Start()
+    {
+        
+        serialPort = new SerialPort("COM3", 9600);
+        serialPort.DtrEnable = true;
+
+        serialPort.RtsEnable = true;
+        while (! serialPort.IsOpen)
+        {
+            print("Try to open the serial port\n");
+            serialPort.Open();
+        }
+
+        print("Serial port opened\n");
+        coordXYZ = this.UpdateCoords();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //this.PrintArduinoOutput();
+        this.updateDirectionCoef();
+    }
+
+    public void PrintArduinoOutput()
+    {
+        print("\n\n In PrintArduinoOutput() \n\n");
+        //string b = serialPort.ReadExisting();
+        string a = serialPort.ReadLine();
+        //print("Output: " + b);
+        print("Output: " + a);
+        //print("Line: " + a + "\tExisting: " + b);
+        //Console.WriteLine(a);
+        //Console.Write("Other one: " + b);
+        //Thread.Sleep(1200);
+        string test = new String(a.Where(Char.IsDigit).ToArray());        
+        string[] test2 = Regex.Split(a.Trim(), @"\D+");
+        test2 = test2.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        print("test: " + test);
+        print("------------------------");
+        for (int i = 0; i < test2.Length; ++i)
+            print(i + "==> " + test2[i]);
+        print("------------------------");
+    }
+
+    public int[] UpdateCoords()
+    {
+        // Get Arduino output           
+        string output = serialPort.ReadLine();
+        // Get the numbers from the output using a regex
+        string[] numData = Regex.Split(output, @"\D+");
+        // Remove potential empty spaces that could appear
+        numData = numData.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+        return Array.ConvertAll(numData, int.Parse);
+    }
+
+    public void updateDirectionCoef()
+    {
+        int[] newCoordXYZ = this.UpdateCoords();
+        int i;
+        int threshold = 100;//2;
+        for(i = 0; i < (newCoordXYZ.Length - 1); ++i)
+        {
+            int result = coordXYZ[i] - newCoordXYZ[i];
+            if (result > threshold)
+                coefXYZ[i] = 1;
+            else if (result < -threshold)
+                coefXYZ[i] = -1;
+            else if (result >= 0 && result <= threshold) 
+                coefXYZ[i] = 0;
+        }
+        coefXYZ[i] = (newCoordXYZ[i] == 0) ? 0 : 1;
+
+        print("OldLen: " + coordXYZ.Length + "\tNewLen: " + newCoordXYZ.Length);
+        for (i = 0; i < newCoordXYZ.Length && i < coordXYZ.Length; ++i)
+        {
+            print(i + ":\t" + coordXYZ[i] + " VS " + newCoordXYZ[i] + "\t==> " + coefXYZ[i]);
+            //print(i + ":\t" +  newCoordXYZ[i]);
+        }
+        print("------------------------");
+
+        /*for (i = 0; i < coordXYZ.Length; ++i)
+        {
+            coordXYZ[i] = newCoordXYZ[i];
+        }
+*/
+    }
+}
---
+++ LabyrinthGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    public Animator animator;
+    public ArduinoReader arduinoReader;
+
+    const float maxSpeedTranslation = 8.5125f;//85.125f;
+    const float minSpeedTranslation = 5.0125f;
+    const float translationStep     = 0.5f;
+
+    const float maxSpeedRotation    = 6.5f;
+    const float minSpeedRotation    = 5.0f;
+    const float rotationStep        = 0.2f;
+
+    static float speedX             = minSpeedRotation;
+    static float speedY             = minSpeedTranslation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        arduinoReader = new ArduinoReader();
+        arduinoReader.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("CALL the move function\n");
+        this.UpdatePosition();
+        arduinoReader.Update(); 
+    }
+
+    void UpdatePosition()
+    {
+
+        /*        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+                float rotation = Input.GetAxis("Horizontal") * speedRotation * Time.deltaTime;
+        */
+        int[] dataXYZ = arduinoReader.coefXYZ;
+
+        this.UpdateSpeed(dataXYZ[1], dataXYZ[0]);
+
+        float translation   = -dataXYZ[1] * speedY * Time.deltaTime;
+        float rotation      = dataXYZ[0] * speedX * Time.deltaTime;
+        
+        this.AnimatorManager(translation, rotation);
+        
+        Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+        rb.MovePosition(rb.position + this.transform.forward * translation);
+        rb.MoveRotation(rb.rotation * turn);
+
+        print("Translation: " + translation + "\tspeedY: " + speedY);
+        print("Rotation: " + rotation + "\tspeedX: " + speedX);
+    }
+
+    public void UpdateSpeed(int coefX, int coefY)
+    {
+        if (coefX == 0)
+            speedX = minSpeedRotation;
+        else if (speedX <= maxSpeedRotation)
+            speedX += rotationStep;
+
+        if (coefY == 0)
+            speedY = minSpeedTranslation;
+        else if (speedY <= maxSpeedTranslation)
+            speedY += translationStep;
+
+    }
+
+    public void AnimatorManager(float translation, float rotation)
+    {
+        if(translation > 0 || rotation > 0)
+        {
+            if(speedX >= (minSpeedTranslation + translationStep * 3f) || speedY >= (minSpeedRotation + rotationStep * 3f))
+            {
+                animator.SetBool("isRunning", true);
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isIdling", false);
+                print("The player is running");
+            }
+            else
+            {
+                animator.SetBool("isWalking", true);
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isIdling", false);
+                print("The player is walking");
+            }
+        }
+        else
+        {
+
+            animator.SetBool("isIdling", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            print("The player is idling");
+        }
+
+        print("********************\nidle: " + animator.GetBool("isIdling") + "\nwalk : " + animator.GetBool("isWalking") + "\nrun : " + animator.GetBool("isRunning") + "\n********************\n");
+    }
+}
